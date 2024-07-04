54cdd18a51e09f4fc52ded7be1fe9c653c735f4f
felipepinho <lfelipepinho@gmail.com>
2024-07-04 15:48:53 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/dtos/CriarClienteDto.java
@@ -0,0 +1,10 @@
+package br.com.cotiinformatica.dtos;
+
+import lombok.Data;
+
+@Data
+public class CriarClienteDto {
+	private String nomeCliente;
+	private String emailCliente;
+	private String telefoneCliente;
+}
